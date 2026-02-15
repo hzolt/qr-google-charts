d81c67a4cee727feb5275421589cce224546ddb7
zoltanh <hegyizoltan.mail@gmail.com>
2026-02-15 08:51:10 +0100

    [MOD] deprecated chart.apis.google.com -> api.qrserver.com [FIX] #copylink value changes

--- inline-qrcode.js
+++ inline-qrcode.js
@@ -1,18 +1,42 @@
 function qrcode_code() {
-  var url = $( '#copylink' ).attr('value');
-  var qrcimg = 'http://chart.apis.google.com/chart?chs=150x150&cht=qr&chld=M&chl='+url;
-  var qrcimglnk = 'http://chart.apis.google.com/chart?chs=500x500&cht=qr&chld=M&chl='+url;	
-  var insertimg = "<div id='qrcode2' class='share'><a href="+qrcimglnk+"><img id='myid' src='" + qrcimg + "' /></a></div>"
-
-  if ( $( '#qrcode2' ).length > 0 ) {
-    $( '#qrcode2' ).remove( );
+  var url = $('#copylink').val();
+  
+  // Remove any existing QR code first
+  $('#qrcode2').remove();
+  
+  if (!url || url === '') {
+    return;
   }
-  if ($( "#shareboxes" )) {  
-    $( "#shareboxes" ).append( insertimg );        // Append new elements
-    $( "div#qrcode2 img" ).css( "width", "100px" );
-    $( "div#qrcode2 img" ).css( "height", "100px" );
+  
+  var qrcimg = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + encodeURIComponent(url);
+  var qrcimglnk = 'https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=' + encodeURIComponent(url);
+  var insertimg = "<div id='qrcode2' class='share'><a href=\"" + qrcimglnk + "\"><img id='myid' src='" + qrcimg + "' /></a></div>";
+  
+  if ($("#shareboxes").length) {
+    $("#shareboxes").append(insertimg);
+    $("div#qrcode2 img").css({"width": "100px", "height": "100px"});
   }
-
 }
 
-$(document).ready( function(){ qrcode_code() } );
+// Watch for changes to the #copylink value
+$(document).ready(function() {
+  // Use MutationObserver to detect when #copylink value changes
+  var observer = new MutationObserver(function(mutations) {
+    qrcode_code();
+  });
+  
+  var copylink = document.getElementById('copylink');
+  if (copylink) {
+    observer.observe(copylink, { attributes: true });
+  }
+  
+  // Also hook into row clicks in the URL table
+  $(document).on('click', '#main_table tr', function() {
+    setTimeout(qrcode_code, 200);
+  });
+  
+  // Clean up when share boxes are hidden
+  $(document).on('click', '.toggle_share', function() {
+    setTimeout(qrcode_code, 200);
+  });
+});
